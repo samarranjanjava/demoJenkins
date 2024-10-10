9c069f1166af6a066bce4452826e56109a3b83ac
SamarRanjan2023 <152635177+SamarRanjan2023@users.noreply.github.com>
2024-10-10 20:20:14 +0530

    New project

---
+++ src/main/java/com/home/demojenkins/DemoJenkins.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.home.demojenkins;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class DemoJenkins {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
